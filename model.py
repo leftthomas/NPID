ebec7c2dfb183bff04403bf77dbea25a92f913ad
leftthomas <leftthomas@qq.com>
2019-12-10 15:42:25 +0800

    change model to resnet18

--- model.py
+++ model.py
@@ -1,21 +1,15 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision.models.resnet import resnet18
 
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, features_dim=128):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.fc1 = nn.Linear(9216, 128)
+        self.features_extractor = resnet18()
+        self.features_extractor.fc = nn.Linear(512, features_dim)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.max_pool2d(x, 2)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.normalize(x)
-        return x
+        features = self.features_extractor(x)
+        features = F.normalize(features)
+        return features
