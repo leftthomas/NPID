6685ae5813bc2ede18f641d0a3aa101367cc7428
leftthomas <leftthomas@qq.com>
2019-12-11 01:10:12 +0800

    fix judge bug

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from torchvision.models.resnet import resnet18, resnet50
 class Net(nn.Module):
     def __init__(self, model_type, features_dim=128):
         super(Net, self).__init__()
-        if model_type is 'resnet18':
+        if model_type == 'resnet18':
             self.features_extractor, expand = resnet18(), 1
         else:
             self.features_extractor, expand = resnet50(), 4
